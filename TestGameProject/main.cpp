e303fc11de21c756e9b4fc82bb9ed4deea60618c
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2025-09-02 03:22:09 +0900

    ビルドできるようなった

--- TestGameProject/main.cpp
+++ TestGameProject/main.cpp
@@ -1,8 +1,8 @@
 #include "CLEYERA.h"
+#pragma comment(lib, "math.lib")
+#pragma comment(lib, "imgui.lib")
+#pragma comment(lib, "DirectXTex.lib")
 
-/// <summary>
-/// メイン関数
-/// </summary>
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
   std::unique_ptr<Engine> engine_ = std::make_unique<Engine>();
