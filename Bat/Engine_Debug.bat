3687875b4fcc2d4c192bf6b4933fd6a56035c273
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2025-09-02 13:29:18 +0900

    bat修正

--- Bat/Engine_Debug.bat
+++ Bat/Engine_Debug.bat
@@ -6,6 +6,7 @@ cd /d "%SCRIPT_DIR%\..\"
 
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
+
 MSBuild "GameEngine\Engine.vcxproj" ^
  /t:clean;rebuild ^
  /p:Configuration=Debug;Platform=x64;ConfigurationType=StaticLibrary ^
@@ -13,9 +14,9 @@ MSBuild "GameEngine\Engine.vcxproj" ^
  /m
 
 
-IF EXIST "Generated\output\Engine\Debug\Engine.lib" (
+IF EXIST "Generated\output\Debug\Engine/Engine.lib" (
     mkdir "Externals\Engine\lib\Debug" >nul 2>&1
-    copy /Y "generated\output\Engine\Debug\Engine.lib" "Externals\Engine\lib\Debug\"
+    copy /Y "Generated\output\Debug\Engine\Engine.lib" "Externals\Engine\lib\Debug\"
 ) ELSE (
     echo Engine.lib not found. Build may have failed.
     pause
@@ -27,3 +28,4 @@ mkdir "%INCLUDE_DIR%" >nul 2>&1
 for /R "GameEngine\" %%f in (*.h) do (
     copy /Y "%%f" "%INCLUDE_DIR%\"
 )
+pause
--- Bat/Run_ALL.bat
+++ Bat/Run_ALL.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
 
+echo Run Start
+echo directory: %cd%
+
+
 rem 
 for %%f in (*.bat) do (
     if /I not "%%~nxf"=="%~nx0" (
@@ -10,5 +14,6 @@ for %%f in (*.bat) do (
 )
 
 endlocal
+
 echo Run_Complite
 pause
